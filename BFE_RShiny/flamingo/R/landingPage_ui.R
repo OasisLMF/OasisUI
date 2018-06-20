9acefccc70532dfc05b2f8b0a86f0f0542f72992
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-06-20 17:16:59 +0200

    rebreanding homepage (issues: #1, #2, #4, #6, #7, #8)

--- BFE_RShiny/flamingo/R/landingPage_ui.R
+++ BFE_RShiny/flamingo/R/landingPage_ui.R
@@ -1,61 +1,62 @@
-
 #' @rdname landingPage
-#' @importFrom shinyBS bsButton
 #' @importFrom DT dataTableOutput
 #' @export
-landingPageUI <- function(id) {
+landingPageUI<- function(id) {
   
   ns <- NS(id)
   
   tagList(
-      fluidRow(
-          column(10,
-              h1("Flamingo 1.0"),
-              h4("Oasis Business Front End")
-          ),
-          column(2,
-              br(),
-              wellPanel(
-                  textOutput(ns("textOutputHeaderData2"))
-              ))
-      ),
-      fluidRow(
-          column(2,
-              bsButton(ns("abuttonexpmngt"), "Exposure Management",
-                  style = "btn btn-primary", size = "default", type = "action",
-                  block = TRUE),
-              bsButton(ns("abuttonprmngt"), "Process Management",
-                  style = "btn btn-primary", size = "default", type = "action",
-                  block = TRUE),
-              bsButton(ns("abuttonfilemngt"), "File Management",
-                  style = "btn btn-primary", size = "default", type = "action",
-                  block = TRUE),
-              bsButton(ns("abuttonsysconf"), "System Configuration",
-                  style = "btn btn-primary", size = "default", type = "action",
-                  block = TRUE),
-              bsButton(ns("abuttonuseradmin"), "User Administration",
-                  style = "btn btn-primary", size = "default", type = "action",
-                  block = TRUE),
-              bsButton(ns("abuttonlogout"), "Logout",
-                  style = "btn btn-primary", size = "default", type = "action",
-                  block = TRUE)
-          ),
-          column(10,
-              
-              wellPanel(
-                  h4("Process Runs Inbox"),
-                  dataTableOutput(ns("tableInbox")),
-                  actionButton(ns("abuttongotorun"), "Goto Run Details",
-                      class = "btn btn-primary", align = "right"),
-                  actionButton(ns("refreshInbox"), "Refresh",
-                      class = "btn btn-primary", align = "right"),
-                  downloadButton(ns("PRIdownloadexcel"),
-                      label="Export to Excel")
-              )),
-          
-          column(12, align = 'right',
-              em("Powered by RShiny", style = "color:gray; font-size:10pt"))
-      )
+             wellPanel(
+                 h4("Process Runs Inbox"),
+                 dataTableOutput(ns("tableInbox")),
+                 actionButton(ns("abuttongotorun"), "Goto Run Details",
+                     class = "btn btn-primary", align = "right"),
+                 actionButton(ns("refreshInbox"), "Refresh",
+                     class = "btn btn-primary", align = "right"),
+                 downloadButton(ns("PRIdownloadexcel"),
+                     label="Export to csv")
+             )
+             # img(src = "landingpage.png", width = "70%") # to be replaced with proper image
   )
+}
+
+#' @rdname pagestructureUI
+#' @importFrom shinyBS bsButton
+#' @importFrom shinyWidgets panel
+#' @export
+pageheaderUI <- function(id) {
   
+  ns <- NS(id)
+  
+  tagList(
+    dropdownButton(label = "accountDD",
+                   circle = TRUE, status = "default",
+                   icon = icon("user"),
+                   size = "s",
+                   textOutput(ns("textOutputHeaderData2")),
+                   bsButton(ns("abuttonuseradmin"), "User Administration",
+                            style = "btn btn-primary", size = "default", type = "action",
+                            block = TRUE),
+                   bsButton(ns("abuttonlogout"), "Logout",
+                            style = "btn btn-primary", size = "default", type = "action",
+                            block = TRUE)
+    )
+  )
+}
+
+
+
+#' @rdname pagestructureUI
+#' @importFrom shinyBS bsButton
+#' @importFrom shinyWidgets actionBttn
+#' @export
+pagestructureUI <- function(id) {
+
+  ns <- NS(id)
+
+  tagList(
+    uiOutput(ns("sidebar"))
+
+   )
+
 }
