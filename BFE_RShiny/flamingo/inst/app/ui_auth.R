ffc88a3d9f1c6ab7d5a91c9f6f2e16f181d20c35
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-06-20 17:21:51 +0200

    rebreanding homepage, sidebar collapsible (issue#32) restructured navigation (issue #33)

--- BFE_RShiny/flamingo/inst/app/ui_auth.R
+++ BFE_RShiny/flamingo/inst/app/ui_auth.R
@@ -1,117 +1,102 @@
 # Flamingo Shiny
-# 
+#
 # (c) 2013-2017 Oasis LMF Ltd.
 # Software provided for early adopter evaluation only.
 ###############################################################################
 
-authUI <- function() {
+authUI <- function(WidthSide = 3, WidthMain = 9) {
+  
   
   tagList(
-      
-      tags$script("Shiny.onInputChange('authUIRenderCallback', true)"),
-      
-      conditionalPanel(
-          condition = "output.menu == 'LP'",
-          landingPageUI("landingPage")
+    
+    tags$script("Shiny.onInputChange('authUIRenderCallback', true)"),
+    tags$style(HTML('table.dataTable tr.selected td, table.dataTable td.selected {background-color: #ebcccc !important;}')),
+    # Header ###
+    fluidRow(
+      column(3,
+             a(href = "https://oasislmf.org",
+               img(src = "OASIS_LMF_COLOUR.png", width = "85%", style = "margin-top:5%"))
       ),
+      column(8, 
+             actionButton(inputId = "abuttoncollapsesidebar", icon = icon("ellipsis-v"), label = NULL),
+             style = "margin-top:2%"),
+      column(1,
+             pageheaderUI("pageheader"),
+             style = "margin-top:2%")
+    ),
+    
+    hr(),
+    
+    # Main body ###
+    fluidRow(
+      # Sidebar panel
+      column(WidthSide,
+             pagestructureUI("pagestructure")),
       
-      
-      ### Exposure Management
-      conditionalPanel(
-          condition = "output.menu == 'EM'",                    
-          navbarPage("Exposure Management", id = "em", 
-              position = "fixed-top",
-              tabPanel("", value = "emtemp"),
-              
-              tabPanel("Define Account",
-                  value = "defineAccount",
-                  accountDefinitionUI("accountDefinition")),
-              
-              tabPanel("Define Programme",
-                  value = "defineProg",
-                  programmeDefinitionUI("programmeDefinition")),
-              
-              tabPanel("Main Menu",  value = "emlp", id = "em-back"),
-              
-              shinyFooter <- div(class = "flamingo-footer", align = "left",
-                                 em("Flamingo 1.0 Oasis Business Front End", style = "color:gray; font-size:10pt; margin-left:1%"),
-                                 em("Powered by RShiny", style = "color:gray; font-size:10pt; float:right; margin-right:2%"))                               
-          ) #End of navbarMenu("Exposure Management")
-      ), #End of conditional panel Exposure Management  
-      
-      
-      ### Process Management 
-      conditionalPanel(
-          condition = "output.menu == 'WF'",                   
-          navbarPage("Process Management", id = "pr",
-              position = "fixed-top",
-              tabPanel("", value = "prtemp"),
-              
-              tabPanel("Process Run",
-                  value = "processrun",
-                  processRunPageUI("processRunPage")),
-              
-              tabPanel("Main Menu",  value = "prlp"),
-              
-              shinyFooter
-          ) #End of navbarMenu("Process Management")
-      ), #End of conditional panel Process Management
-      
-      #### File Management
-      conditionalPanel(
-          condition = "output.menu == 'FM'",
-          navbarPage("File Management", id = "fm",
-              position = "fixed-top",
-              tabPanel("", value = "fmtemp"),
-              
-              tabPanel("File Viewer",
-                  value = "fileviewer",
-                  fileViewerUI("fileViewer")),
-              
-              tabPanel("Main Menu",  value = "fmlp"),
-              
-              shinyFooter
-          ) # End of navbarMenu(File Management")
-      ), # End of conditional panel File Management
-      
-      ### System Config
-      conditionalPanel(
-          condition = "output.menu == 'SC'",
-          navbarPage("System Configuration", id = "sc",
-              position = "fixed-top",
-              tabPanel("", value = "sctemp"),
-              
-              tabPanel("Model",
-                  value = "Model",
-                  modelSupplierPageUI("modelSupplierPage")),
-              
-              tabPanel("Main Menu",  value = "sclp"),
-              
-              shinyFooter
-          ) # End of navbarMenu(System Config)
-      ), # End of conditional panel System Config
-      
-      ### User Admin 
-      conditionalPanel(
-          condition = "output.menu == 'UA'",                    
-          navbarPage("User Administration", id = "ua",
-              position = "fixed-top",
-              tabPanel("", value = "uatemp"),
-              
-              tabPanel("Company",
-                  value = "definecompany",
-                  companyDefinitionUI("companyDefinition")),
-              
-              tabPanel("Company User Administraton",
-                  value = "defineuser",
-                  userAdminDefinitionUI("userAdminDefinition")),
-              
-              tabPanel("Main Menu",  value = "ualp"),
-              
-              shinyFooter                            
-          ) # End of navbarMenu("User Administration")
-      ) # End of conditional panel User Administrationt  
-  
-  ) # End of conditiopnalPanel Utilities                   
-}
+      #Main panel
+      column(WidthMain,
+            
+            ### Landing Page
+            conditionalPanel(
+              condition = "output.menu == 'LP'",
+              #img(src = "landingpage.png", width = "70%")
+              landingPageUI("landingPage")
+            ),
+            
+            ### Exposure Management
+            conditionalPanel(
+              condition = "output.menu == 'EM'",
+              #navbarPage("Exposure Management", id = "em",
+              tabsetPanel(id = "em",
 
+                         tabPanel("Define Account",
+                                  value = "defineAccount",
+                                  accountDefinitionUI("accountDefinition")),
+                         
+                         tabPanel("Define Programme",
+                                  value = "defineProg",
+                                  programmeDefinitionUI("programmeDefinition"))
+              ) #End of navbarMenu("Exposure Management")
+            ), #End of conditional panel Exposure Management
+            
+            
+            ### Process Management
+            conditionalPanel(
+              condition = "output.menu == 'WF'",
+              processRunPageUI("processRunPage")), #End of conditional panel Process Management
+            
+            #### File Management
+            conditionalPanel(
+              condition = "output.menu == 'FM'",
+              fileViewerUI("fileViewer")), # End of conditional panel File Management
+            
+            ### System Config
+            conditionalPanel(
+              condition = "output.menu == 'SC'",
+              modelSupplierPageUI("modelSupplierPage")), # End of conditional panel System Config
+            
+            ### User Admin
+            conditionalPanel(
+              condition = "output.menu == 'UA'",
+              #navbarPage("User Administration", id = "ua",
+              tabsetPanel(id = "ua",
+                          tabPanel("Company",
+                                  value = "definecompany",
+                                  companyDefinitionUI("companyDefinition")),
+                         
+                         tabPanel("Company User Administraton",
+                                  value = "defineuser",
+                                  userAdminDefinitionUI("userAdminDefinition"))
+              ) # End of tabsetpanel
+            ) # End of conditional panel User Administration
+            
+          ) # endo of column
+    ), # end of fluidRow
+    
+    # Footer ###
+    fillRow(
+      em("Flamingo 1.1 Oasis Business Front End", style = "color:gray; font-size:10pt; margin-left:5%"),
+      em("Powered by RShiny", style = "color:gray; font-size:10pt; float:right; margin-right:2%"), align = "left")
+  )
+  
+}
