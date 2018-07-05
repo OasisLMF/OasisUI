47beeaae9338451053edc75fd6ba22da0d928702
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-07-05 16:37:21 +0200

    issue #24 Simplify File Views - Per-file action buttons to view file / map

--- BFE_RShiny/flamingo/R/fileViewer_ui.R
+++ BFE_RShiny/flamingo/R/fileViewer_ui.R
@@ -4,94 +4,33 @@
 #' @import leaflet
 #' @importFrom DT dataTableOutput
 #' @importFrom shinyjs hidden disabled
+#' @importFrom shinyBS bsTooltip
 #' @export
 fileViewerUI <- function(id) {
   
   ns <- NS(id)
-  
-  jsPanelCondition <- function(id, value) {
-    paste0("output['", ns(id), "']", " == ", "'", value, "'")
-  }
-  
+
   attachDependencies(value = flamingoHtmlDependencies(), tagList(
       
       h3("File Viewer", class = "flamingo-page-title"),
-      
-      hidden(
-          verbatimTextOutput(ns("FVPanelSwitcher"))
-      ),
-      
-      actionButton(ns(fileMgmtButtons[["FOBtnShowTable"]]), "Table", 
-          class = "btn btn-primary"),
-      disabled(
-          actionButton(ns(fileMgmtButtons[["FOBtnShowRawContent"]]), "File",
-              class = "btn btn-primary")),
-      disabled(
-          actionButton(ns(fileMgmtButtons[["FOBtnShowMap"]]), "Map",
-              class = "btn btn-primary")),
-      # actionButton(ns(noteButtonID(FOBtnShowAEPCurve)), "AEP Curve", class = "btn btn-primary"),
-      # actionButton(ns(noteButtonID(FOBtnShowGeocode)),        "Geocode",         class = "btn btn-primary"),
-      # actionButton(ns(noteButtonID(FOBtnShowEventFootprint)), "Event Footprint", class = "btn btn-primary"),
-      
-      conditionalPanel(
-          condition = jsPanelCondition("FVPanelSwitcher", "FO_div_table"),
-          
+
           h4("File List", class = "flamingo-table-title"),
-#            column(12, actionButton("abuttonprocessinfo", "Process Run Details", class="btn btn-primary"), align = "right"),
-#            #bsModal("modalExample", "Data Table", "tabBut", size = "large",dataTableOutput("distTable")),
+
+          checkboxInput(inputId = ns("tableFVfileListSelectall"), label = "Select all", value = FALSE),
           dataTableOutput(ns("tableFVfileList")),
-          hidden(                           
-              div(id = ns("processruninfodiv"),  
-                  h4("Process Run Details", class = "flamingo-table-title"),
-                  htmlOutput(ns("textprcruninfo")),
-                  htmlOutput(ns("textprcrunparaminfo"))
-              ))
-      ),
-      
-      
-      conditionalPanel(
-          condition = jsPanelCondition("FVPanelSwitcher", "FO_div_filecontents"),
-          
-          h4("File Contents", class = "flamingo-table-title"),
-          dataTableOutput(ns("tableFVExposureSelected")),
-          br(),
-          downloadButton(ns("FVEdownloadexcel"), label = "Export to Excel")
-      ),
-      
-      
-      conditionalPanel(
-          condition = jsPanelCondition("FVPanelSwitcher", "FO_div_plainmap"),
-    
-          h4("Map", class = "flamingo-table-title"),
-          leafletOutput(ns("plainmap"))
-      ),
-      
-      
-      conditionalPanel(
-          condition = jsPanelCondition("FVPanelSwitcher", "FO_div_AEPcurve"),
-          h4("AEP Curve", class = "flamingo-table-title"),
-          plotOutput(ns("plotFVAEPCurve"))
-          #,
-          #    h4("AEP Curve Data"),
-          # TODO XXX commented out for serious login-page-not-appearing shiny bug 
-          #    DT::dataTableOutput("tableFVAEPdata"),
-          #    downloadButton("FVAEPdownloadexcel",label="Export to Excel")
-      ),
-      
-      conditionalPanel(
-          condition = jsPanelCondition("FVPanelSwitcher", "FO_div_geocode"),
-          h4("Geocoded Data", class = "flamingo-table-title"),
-          dataTableOutput(ns("tableGeocodeData"))
-          # ,
-          # TODO XXX commented out for serious login-page-not-appearing shiny bug 
-          #    downloadButton("FVGdownloadexcel",label="Export to Excel")
-      ),
-      
-      conditionalPanel(
-          condition = jsPanelCondition("FVPanelSwitcher", "FO_div_EventFootprint"),
-          h4("Event Footprint", class = "flamingo-table-title"),
-          leafletOutput(ns("EventFootprintMap"))
-      )
+
+          # textOutput(ns("rowsSelected")),
+
+          downloadButton(ns("FVfileListdownloadzip"), label = "Export to zip"),
+          bsTooltip(ns("FVfileListdownloadzip"), 
+                    file_Viewer$FVfileListdownloadzip,
+                    placement = "right", 
+                    options   = list(container = "body")),
+          downloadButton(ns("FVFLdownloadexcel"), label = "Export to csv"),
+          bsTooltip(ns("FVFLdownloadexcel"), 
+                      file_Viewer$FVFLdownloadexcel, 
+                      placement = "right", 
+                      options   = list(container = "body"))
   
   ))
   
