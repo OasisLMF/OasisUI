d6a5b0f3adec659c15a334213dc25ee4e30d0ccb
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-07-05 09:23:24 +0200

    issue #11 added file with descriptions for mouse-overs

---
+++ BFE_RShiny/flamingo/inst/app/helper_text.R
@@ -0,0 +1,17 @@
+# Helper texts to show explanations next to buttons. 
+# A list per file. 
+# Naming convention: buttonname
+
+file_Viewer <- list(
+  FVfileListdownloadzip = "Download all checked files",
+  FVFLdownloadexcel = "Download files table"
+)
+
+landing_page <- list(
+  abuttongotorun = "Go to run details",
+  PRIdownloadexcel = "Download files table",
+  abuttonexpmngt = "Define the exposure inputs",
+  abuttonprmngt = "Define programme details",
+  abuttonfilemngt = "Access input/output files",
+  abuttonsysconf = "System configuration"
+)
