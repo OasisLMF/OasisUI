8e4d3f367aa7fbb46e9f789f500e4405a22a5f40
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-07-23 13:38:04 +0200

    issue #35 placeholder for new section Run/Batch

---
+++ BFE_RShiny/flamingo/R/programmeDefinitionBatch_server.R
@@ -0,0 +1,25 @@
+#' Batch Programme Definition Module
+#' @description Server logic to define a programme
+#' @inheritParams flamingoModule
+#' @param reloadMillis amount of time to wait between table updates;
+#' see \link{invalidateLater}; 
+#' @return list of reactives:
+#' @rdname programmeDefinitionBatch
+#' @importFrom shinyjs show hide enable disable
+#' @importFrom DT renderDataTable
+#' @importFrom dplyr mutate
+#' @export
+programmeDefinitionBatch <- function(input, output, session, dbSettings,
+                                     apiSettings, userId, active = reactive(TRUE), logMessage = message,
+                                     preselRunId = reactive(-1),
+                                     preselProcId = reactive(-1),
+                                     reloadMillis = 10000) {
+  
+  ns <- session$ns
+  
+  ### Module Output
+  
+  moduleOutput <- list()
+  
+  return(moduleOutput)
+}
---
+++ BFE_RShiny/flamingo/R/programmeDefinitionBatch_ui.R
@@ -0,0 +1,11 @@
+#' @rdname programmeDefinitionBatch
+#' @description UI/View for the process run page
+#' @inheritParams flamingoModuleUI
+#' @return list of tags
+#' @importFrom DT dataTableOutput
+#' @export
+programmeDefinitionBatchUI <- function(id) {
+  
+  ns <- NS(id)
+  
+}
