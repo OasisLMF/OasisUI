27d8b9377a0655ae7c4ef1ab54b95727b87f31a8
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-07-23 12:54:10 +0200

    issues #35 - #41 extended helper texts

--- BFE_RShiny/flamingo/inst/app/helper_text.R
+++ BFE_RShiny/flamingo/inst/app/helper_text.R
@@ -8,10 +8,23 @@ file_Viewer <- list(
 )
 
 landing_page <- list(
+  abuttonrun = "Define account and programme to run",
+  abuttonbrowse = "Visualize outputs",
+  abuttondefineaccount = "Create/amend/delete account",
+  abuttondefineprogramme = "Create/amend/delete Programme; Associate Model; Run Model",
+  abuttondefineprogrammesingle = "Define single Programme Run",
+  abuttondefineprogrammebatch = "Define batch Programeme Run",
   abuttongotorun = "Go to run details",
   PRIdownloadexcel = "Download files table",
   abuttonexpmngt = "Define the exposure inputs",
   abuttonprmngt = "Define programme details",
   abuttonfilemngt = "Access input/output files",
   abuttonsysconf = "System configuration"
+)
+
+programme_Definition_Single <- list(
+  sinputTransformname = "Transformation Description to be added here",
+  sinputProgModTransform = "Transformation Description to be added here",
+  selectprogammeID = "Select a programme ID to which associate a model",
+  selectprogOasisID = "Select the Oasis Programme to Run"
 )
