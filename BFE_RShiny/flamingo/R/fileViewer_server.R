360866485ab5a0096d6aef3c6780515394320341
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-08-21 14:45:32 +0200

    fixes issue #47 for fileviewer

--- BFE_RShiny/flamingo/R/fileViewer_server.R
+++ BFE_RShiny/flamingo/R/fileViewer_server.R
@@ -23,9 +23,7 @@ fileViewer <- function(
     FLdata = NULL,               #table of files
     FVid = -1,                   #needed to get the operations valid on file id
     fileData = NULL,             #file to download
-    lastViewBtn = character(),   #last button View to be clicked
-    lastMapBtn  = character(),   #last button Map to be clicked
-    validButtons = NULL,         #buttons for which the user has permission. Depends on current list of files
+    currentFile = NULL,          #Current filename
     lenFLdata = 400              #length of the table fileData
   )
   
@@ -208,7 +206,7 @@ fileViewer <- function(
   
   # Export to .csv
   output$FVEdownloadexcel <- downloadHandler(
-    filename = paste0(result$FLdata[result$lastViewClicked, 3]),
+    filename = result$currentFile, 
     content = function(file) {
       write.csv(result$fileData, file)}
   )
@@ -237,7 +235,7 @@ fileViewer <- function(
     FUN = function(i){
       observeEvent(input[[paste0("vrows_", i)]], {
         if (input[[paste0("vrows_", i)]] > 0) {
-          result$lastViewBtn <- paste0(i)   
+          result$currentFile <-  paste0(result$FLdata[i, 2])
           showModal(FileContent)
           # Extra info table
           output$tableFVExposureSelectedInfo <- renderUI({
@@ -279,7 +277,6 @@ fileViewer <- function(
     FUN = function(i){
       observeEvent(input[[paste0("mrows_", i)]], {
         if (input[[paste0("mrows_", i)]] > 0) {
-          result$lastMapBtn <- paste0(i)   
           showModal(Map)
           fileName <- file.path(result$FLdata[i, 5], result$FLdata[i, 2])
           # Extra info table
