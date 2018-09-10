b9f4e231e982952884d2c9a8f0718a3f89bbcfaf
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2018-09-10 14:37:41 +0200

    Split reactivity * Cleaner, more efficient. * No reactiveValues state.

--- BFE_RShiny/flamingo/R/landingPage_server.R
+++ BFE_RShiny/flamingo/R/landingPage_server.R
@@ -19,43 +19,39 @@
 landingPage <- function(input, output, session, userId, userName, dbSettings,
                         reloadMillis = 10000, logMessage = message, active = reactive(TRUE)) {
 
+  # navigation -----
   navigation_state <- reactiveNavigation()
-
-  result <- reactiveValues(
-    inbox = NULL,
-    runIdList = NULL
-  )
-
   observeEvent(input$abuttongotorun,
                updateNavigation(navigation_state, "SBR"))
 
-  observe(if (active()) {
+  inbox <- reactive({
+    if (active()) {
+      # reload automatically every so often
+      invalidateLater(reloadMillis) # does not seem to work as of 10/09/2018
+      # explicit refresh button
+      input$refreshInbox
+      logMessage("refreshing inbox...")
+      data <- getInboxData(dbSettings, userId())
+      data %>%
+        mutate(Status = replace(Status, Status == "Failed" | Status == "Cancelled", StatusFailed)) %>%
+        mutate(Status = replace(Status, Status == "Completed", StatusCompleted)) %>%
+        mutate(Status = replace(Status, Status != "Completed" & Status != "Failed" & Status != "Cancelled" & Status != StatusFailed & Status != StatusCompleted, StatusProcessing)) %>%
+        as.data.frame()
+    }
+  })
 
+  observe(if (active()) {
+    logMessage("refreshing permissions")
     # invalidate if the refresh button updates
     force(input$refreshInbox)
-
     # reload automatically every so often
-    invalidateLater(reloadMillis)
-
+    invalidateLater(reloadMillis, session)
     landingPageButtonUpdate(session, dbSettings, userId())
-
-    data <- getInboxData(dbSettings, userId())
-    result$inbox <-  data %>%
-      mutate(Status = replace(Status, Status == "Failed" | Status == "Cancelled", StatusFailed)) %>%
-      mutate(Status = replace(Status, Status == "Completed", StatusCompleted)) %>%
-      mutate(Status = replace(Status, Status != "Completed" & Status != "Failed" & Status != "Cancelled" & Status != StatusFailed & Status != StatusCompleted, StatusProcessing)) %>%
-      as.data.frame()
-
-    logMessage("inbox refreshed")
-
-    result$runIdList <- result$inbox[, c("RunID", "Status")]
-    #logMessage(paste0("result$runIdList ", names(result$runIdList)))
   })
 
   output$tableInbox <- renderDataTable(if (userId() != FLAMINGO_GUEST_ID) {
-
     datatable(
-      result$inbox,
+      inbox(),
       class = "flamingo-table display",
       rownames = TRUE,
       selection = "single",
@@ -68,13 +64,12 @@ landingPage <- function(input, output, session, userId, userName, dbSettings,
         columnDefs = list(list(visible = FALSE, targets = 0, type = 'natural'))
       )
     )
-
   })
 
   output$PRIdownloadexcel <- downloadHandler(
     filename = "processruninbox.csv",
     content = function(file) {
-      write.csv(result$inbox, file)
+      write.csv(inbox(), file)
     }
   )
 
@@ -83,13 +78,12 @@ landingPage <- function(input, output, session, userId, userName, dbSettings,
     outputNavigation(navigation_state),
     list(
       runId = reactive(if (length(i <- input$tableInbox_rows_selected) == 1) {
-        result$inbox[i, 2]} else -1),
+        inbox()[i, 2]} else -1),
       # this is needed in processRun, probably shouldn't
       procId = reactive(if (length(i <- input$tableInbox_rows_selected) == 1) {
-        result$inbox[i, 1]} else -1),
-      runIdList = reactive(result$runIdList)
+        inbox()[i, 1]} else -1),
+      runIdList = reactive(inbox()[, c("RunID", "Status")])
     )
-
   )
 
   moduleOutput
