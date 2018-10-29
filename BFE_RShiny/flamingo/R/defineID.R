b079f3939feca05a10bed9c6c375c4e2a80595c2
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-10-29 18:24:41 +0100

    issue #108 pick run from filtered table. Provide more info to the user. Make clear that a run should be selected

--- BFE_RShiny/flamingo/R/defineID.R
+++ BFE_RShiny/flamingo/R/defineID.R
@@ -6,6 +6,7 @@
 #' @param w width of the coulmn
 #' @param i counter
 #' @importFrom bsplus bs_embed_tooltip
+#' @importFrom shinyjs hidden
 #' @export
 defineIDUI <- function(id, w, batch = FALSE){
   ns <- NS(id)
@@ -16,19 +17,19 @@ defineIDUI <- function(id, w, batch = FALSE){
   }
   
   column(w,
-         # selectInput(inputId =  ns(paste0("selectRunID")), label = labelrun, choices = "", selected = NULL) %>%
-         #   bs_embed_tooltip(title = browse_programmes$selectRunID, placement = "right")
-         uiOutput(ns("selectRunInfo")),
          actionButton(ns(paste0("chooseRunID")), label = NULL, icon = icon("list-alt"), 
-                                style = " color: black;
-                                          background-color: white;
-                                          padding: 0px;
-                                          font-size: 24px;
-                                          background-image: none;
-                                          border: none;
+                      style = " color: rgb(71, 73, 73);
+                               background-color: white;
+                               padding: 0px;
+                               font-size: 24px;
+                               background-image: none;
+                               border: none;
                                 ") %>%
-           bs_embed_tooltip(title = browse_programmes$selectRunID, placement = "right")
-         )
+           bs_embed_tooltip(title = browse_programmes$selectRunID, placement = "right"),
+         div(textOutput(ns("selectRunInfo1"), inline = TRUE), style = "font-weight:bold; font-color: #2d2d2d; display:inline;"),
+         div(textOutput(ns("selectRunInfo2"), inline = TRUE), style = "font-weight:bold; font-color: #2d2d2d; display:inline; 
+                                                                       padding:10px; margin: 5px; border-style: ridge;"),
+         style = "display:inline;")
   
 }
 
@@ -40,12 +41,13 @@ defineIDUI <- function(id, w, batch = FALSE){
 #' @param runIdList list of runs and their status
 #' @param preselectedRunId reactive string expression for reselected run id from landingpage
 #' @return reactive for runID selected 
-#' @importFrom dplyr mutate select contains filter
+#' @importFrom shinyjs hide show
 #' @export
 defineID <- function(input, output, session, 
                      dbSettings, userId, 
                      runIdList = reactive(c(-1)),
                      preselectedRunId = reactive(-1),
+                     batch = FALSE,
                      logMessage = message) {
   
   ns <- session$ns
@@ -54,75 +56,108 @@ defineID <- function(input, output, session,
   result <- reactiveValues(
     inbox = NULL,
     selectRunID = "",
-    selectRunName = ""
+    selectRunName = "",
+    preselRow = NULL
   )
   
   # list of sub-modules
   sub_modules <- list()
-
-  # Update RunID----------------------------------------------------------------
   
-  # observeEvent(preselectedRunId(), {
-  #   index <- match(c(preselectedRunId()), runIdList()$RunID)
-  #   if (!is.null(index) & !is.na(index)) {
-  #     updateSelectInput(session, inputId = "selectRunID", choices = runIdList()$RunID, selected = runIdList()$RunID[index])
-  #   }
-  # })
+  #label
+  labelrun <- "Run"
+  if (batch) {
+    labelrun <- "Batch"
+  }
   
-  # observeEvent(input[["selectRunID"]], ignoreInit = TRUE, {
-  #   result$selectRunID <- input[["selectRunID"]]
-  # })
+  # Modal for RunID selection --------------------------------------------------
   
+  # > Modal Panel
+  RunsList <- modalDialog(
+    easyClose = TRUE,
+    size = "l",
+    flamingoTableUI(ns("tableInboxpanel")),
+    footer = tagList(
+      flamingoButton(ns("abuttonselectRun"),
+                     label = "Select Run", align = "left"),
+      actionButton(ns("abuttonccancel"),
+                   label = "Cancel", align = "right")
+    )
+  )
+  
+  # > open modal
   observeEvent(input$chooseRunID, {
     data <- getInboxData(dbSettings, userId())
     result$inbox <- data  %>%
       replaceWithIcons() %>% 
       filter(Status == StatusCompleted)
     showModal(RunsList)
-    sub_modules$tableInbox <- callModule(
-      flamingoTable,
-      id = "tableInbox",
-      data = reactive(data),
-      selection = "single",
-      escape = FALSE,
-      scrollX = TRUE,
-      filter = FALSE,
-      rownames = TRUE,
-      colnames =  c("Row Number" = 1),
-      maxrowsperpage = 10,
-      logMessage = logMessage)
-    
-    print(names(result$inbox))
   })
   
-  observeEvent(input$selectRun, {
-    if (!is.null(result$inbox)) {
-      result$selectRunID <- result$inbox[input$tableInbox_selected_rows,1]
-      result$selectRunName <- result$inbox[input$tableInbox_selected_rows,2]
+  # > modal content
+  sub_modules$tableInboxpanel <- callModule(
+    flamingoTable,
+    id = "tableInboxpanel",
+    data = reactive(result$inbox),
+    selection = "single",
+    escape = FALSE,
+    scrollX = TRUE,
+    filter = FALSE,
+    rownames = FALSE,
+    colnames =  c("Row Number" = 1),
+    preselRow = reactive({result$preselRow}),
+    maxrowsperpage = 10,
+    logMessage = logMessage)
+  
+  # > row to select
+  observeEvent(preselectedRunId(), {
+    result$preselRow <- match(preselectedRunId(), result$inbox[, inbox.RunID])
+  })
+  
+  
+  # > select run ID
+  observeEvent(sub_modules$tableInboxpanel$rows_selected(), ignoreNULL = FALSE, {
+    currid <- ""
+    currName <- ""
+    if (!is.null(sub_modules$tableInboxpanel$rows_selected())) {
+      currid <- result$inbox[sub_modules$tableInboxpanel$rows_selected(),inbox.RunID]
+      currName <- result$inbox[sub_modules$tableInboxpanel$rows_selected(),inbox.RunName]
     }
-
+    result$selectRunID <- ifelse(is.null(currid) | is.na(currid), "", currid)
+    result$selectRunName <-  ifelse(is.null(currName) | is.na(currName), "", currName)
   })
   
-  # Modal Panel
-  RunsList <- modalDialog(
-    easyClose = TRUE,
-    size = "l",
-    flamingoTableUI(ns("tableInbox")),
-    flamingoButton(ns("selectRun"), label = "Select Run")
-  )
+  # > close modal
+  observeEvent(input$abuttonccancel, {
+    removeModal()
+  })
   
-  # text infos on Run ID
-  output$selectRunInfo <- renderUI(h5(paste0("Selected Run ", result$selectRunID, " " ,result$selectRunName)))
+  observeEvent(input$abuttonselectRun, {
+    if (!is.null(sub_modules$tableInboxpanel$rows_selected())) {
+      result$preselRow <- sub_modules$tableInboxpanel$rows_selected()
+    }
+    removeModal()
+  })
+  
+  output$selectRunInfo1 <- renderText({
+    if (result$selectRunID == "") {
+      info <- paste0("Select ", labelrun, ":   ")
+    } else {
+      info <- paste0('Selected ', labelrun, ': ')
+    }
+    info
+  })
+  
+  output$selectRunInfo2 <- renderText({
+    if (result$selectRunID == "") {
+      info <- " ... "
+    } else {
+      info <- paste0(result$selectRunID, ' "' ,result$selectRunName, '"  ')
+    }
+    info
+  })
   
   # Module Outout --------------------------------------------------------------
-  
-  selectRunID <- reactive({
-    if ((is.null(result$selectRunID) | is.na(result$selectRunID))) {
-      result$selectRunID <-  ""
-    } else {
-      result$selectRunID <- result$selectRunID
-    }
-  })
+  selectRunID <- reactive({ifelse(is.null(result$selectRunID) | is.na(result$selectRunID), "", result$selectRunID)})
   
   moduleOutput <- c(
     list(
