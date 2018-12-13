804cf2ddc2df2ecb9620142e9dd259bb47cada5d
fvitalini <francesca.vitalini@mirai-solutions.com>
2018-12-13 09:02:12 +0100

    improved documentation

--- BFE_RShiny/flamingo/R/flamingo-package.R
+++ BFE_RShiny/flamingo/R/flamingo-package.R
@@ -1,26 +1,6 @@
-#' Pipe operator
-#'
-#' @rdname pipe
-#'
-#' @description pipe operator imported from dplyr to avoid unncessary roxygen tags
-#' 
-#' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using the magrittr semantics.
-#'
 #' @export
-#'
-#' @md
 '%>%' <- dplyr::'%>%'
 
-#' Negate in operator
-#'
-#' @rdname negate
-#'
-#' @description negate in operator
-#' 
-#' @param ... extra arguments to [Negate()]
-#'
+
 #' @export
-#'
-#' @md
 '%notin%' <- Negate('%in%')
