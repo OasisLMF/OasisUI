90bed48e114220c832bbbb96a0d898e75c9dec3e
fvitalini <francesca.vitalini@mirai-solutions.com>
2019-01-09 15:21:24 +0100

    using conventional approach to require readr

--- BFE_RShiny/flamingo/R/flamingo-package.R
+++ BFE_RShiny/flamingo/R/flamingo-package.R
@@ -4,3 +4,8 @@
 
 #' @export
 '%notin%' <- Negate('%in%')
+
+# Note readr is not needed directly, but required by httr to parse csv files
+# (httr has it in the 'Suggests' field only).
+#' @importFrom readr read_csv
+"_PACKAGE"
