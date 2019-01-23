737e00c6e3ac2b3da56eea19f274a0eacf53ad2b
fvitalini <francesca.vitalini@mirai-solutions.com>
2019-01-23 18:28:09 +0100

    issue #136 : improve tooltip text and remove obsolete tooltips

--- BFE_RShiny/flamingo/inst/app/helper_text.R
+++ BFE_RShiny/flamingo/inst/app/helper_text.R
@@ -3,77 +3,77 @@
 # Naming convention: buttonname
 
 file_Viewer <- list(
-  FLdownloadzip = "Download all selected files."
+  FLdownloadzip = "Download all selected files"
   #FVFLdownloadexcel = "Download files table"
 )
 
 landing_page <- list(
-  abuttonanalysis = "Define analysis to run.",
-  abuttonbrowse = "Choose browse option.",
-  abuttondefineaccount = "Create/amend/delete account.",
-  abuttondefineanasingle = "Define single analysis.",
-  abuttondefineanabatch = "Define batch analysis.",
-  abuttonbrowseSBR = "Visualize outputs from single analysis.",
-  abuttonbrowseBBR = "Visualize results across analyses or from batch runs.",
-  abuttonbrowseCBR = "Compare runs.",
-  abuttongotoana = "Go to analysis details.",
-  abuttondelana = "Delete selected analysis.",
-  downloadexcel_ana = "Download analyses table.",
-  abuttonfilemngt = "Access input/output files.",
-  abuttonsysconf = "System configuration.",
-  accountDD = "User info.",
-  abuttonuserupdate = "Update selected user.",
-  abuttonuserdelete = "Delete selected user.",
-  abuttonusersecurity = "Add security group to selected user.",
-  abuttonuseroasis = "Add license to selected user."
+  abuttonanalysis = "Define analysis to run",
+  abuttonbrowse = "Visualize outputs",
+  abuttondefineaccount = "Create/amend/delete account",
+  abuttondefineanasingle = "Define single analysis",
+  abuttondefineanabatch = "Define batch analysis",
+  abuttonbrowseSBR = "Visualize outputs from single analysis",
+  abuttonbrowseBBR = "Visualize results across analyses or from batch runs",
+  abuttonbrowseCBR = "Compare runs",
+  abuttongotoana = "Visualize outputs of selected analysis",
+  abuttondelana = "Delete selected analysis",
+  downloadexcel_ana = "Download analyses table",
+  abuttonfilemngt = "Access input/output files",
+  abuttonsysconf = "System configuration",
+  accountDD = "User information",
+  abuttonlogout = "Logout the application. You will need to sign back in",
+  abuttonuserupdate = "Update selected user",
+  abuttonuserdelete = "Delete selected user",
+  abuttonusersecurity = "Add security group to selected user",
+  abuttonuseroasis = "Add license to selected user"
 )
 
-  define_account <- list(
-    abuttonamendac = "Amend selected account.",
-    abuttondeleteac = "Delete selected account."
-  )
+define_account <- list(
+  abuttonamendac = "Amend selected account",
+  abuttondeleteac = "Delete selected account"
+)
 
-  define_company <- list(
-    abuttoncompupdate = "Update selected company.",
-    abuttoncompdel = "Delete selected company."
-  )
+define_company <- list(
+  abuttoncompupdate = "Update selected company",
+  abuttoncompdel = "Delete selected company"
+)
 
-  sys_conf <- list(
-    abuttonamend = "Amend selected model resource.",
-    abuttondelete = "Delete selected model resource.",
-    abuttonConfirmDel = "Delete selected model resource.",
-    abuttonSubmitCrtAm = "Submit Programme."
-  )
+sys_conf <- list(
+  abuttonamend = "Amend selected model resource",
+  abuttondelete = "Delete selected model resource",
+  abuttonConfirmDel = "Delete selected model resource",
+  abuttonSubmitCrtAm = "Submit Programme"
+)
 
 defineSingleAna <- list(
-  abuttoncreateana = "Create a new analysis.",
-  abuttonshowanadetails = "Show details for selected analysis.",
-  abuttonuploadsourcefiles = "Upload source files to selected portfolio.",
-  portfolioID = "Select a portfolio ID to which associate a model.",
-  modelID = "Select the model to associate with the portfolio.",
-  abuttonpfdetails = "Show details for selected portfolio.",
-  abuttonamendpf = "Amend selected portfolio.",
-  abuttonpfsubmit = "Submit new portfolio.",
-  abuttondeletepf = "Delete selected portfolio.",
-  abuttonmodeldetails = "Show details for selected portfolio.",
-  abuttonsaveoutput = "Save selected output configuration.",
-  abuttonexecuteanarun = "Execute run on selected output configuration.",
-  abuttonshowlog = "Show log on selected analysis.",
-  abuttondisplayoutput = "Display output for selected analysis.",
-  abuttoncancelana = "Cancel selected analysis.",
-  abuttonConfirmDel = "Confirm deleting analysis.",
-  abuttoncancelIG = "Stop input generation on selected analysis.",
-  abuttonstartIG = "Start input generation.",
-  abuttonshowIG = "Visualize the generated inputs for selected analysis.",
-  abuttonConfirmDelIG = "Confirm cancelling input generation.",
-  abuttonrunboth = "Run Output Configuration"
+  abuttoncreateana = "Create new analysis",
+  abuttonshowanadetails = "Show details for selected analysis",
+  abuttonuploadsourcefiles = "Upload source files to selected portfolio",
+  portfolioID = "Select a portfolio ID",
+  abuttonpfdetails = "Show details for selected portfolio",
+  abuttonamendpf = "Amend selected portfolio",
+  abuttonpfsubmit = "Submit new portfolio",
+  abuttondeletepf = "Delete selected portfolio",
+  abuttonmodeldetails = "Show details for selected model",
+  #abuttonsaveoutput = "Save selected output configuration",
+  abuttonexecuteanarun = "Execute run with given output configuration",
+  abuttonshowlog = "Show log for selected analysis",
+  abuttondisplayoutput = "Visualize outputs of selected analysis",
+  abuttoncancelana = "Cancel selected analysis",
+  abuttonConfirmDel = "Confirm deleting analysis",
+  abuttoncancelIG = "Interrupt input generation for selected analysis",
+  abuttonstartIG = "Start input generation for selected analysis",
+  abuttonshowIG = "Visualize the generated inputs for selected analysis",
+  abuttonConfirmDelIG = "Confirm interruption input generation",
+  abuttonrunconfig = "Define output configuration and run analysis"
 )
 
 ### Panel Browse ----
 
 dashboard <- list(
-  selectAnaID = "Select analysis ID.",
-  selectBatchAnaID = "Select Batch analysis ID.",
-  abuttonplusplot = "Add a new plot.",
-  abuttonselectAna = "Select analysis."
-  )
+  selectAnaID = "Select analysis ID",
+  selectBatchAnaID = "Select Batch analysis ID",
+  abuttonplusplot = "Add a new plot",
+  abuttonselectAna = "Select analysis"
+)
