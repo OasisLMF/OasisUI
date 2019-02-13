d20a2194fa640bc3c94814b066af4c8412b82f44
fvitalini <francesca.vitalini@mirai-solutions.com>
2019-02-13 15:47:39 +0100

    improved comments

--- BFE_RShiny/flamingo/R/flamingo-package.R
+++ BFE_RShiny/flamingo/R/flamingo-package.R
@@ -6,12 +6,14 @@
 '%notin%' <- Negate('%in%')
 
 # Note: readr is not needed directly, but required by httr to parse csv files
-# (httr has it in the 'Suggests' field only).
-# Note: shiny >= 1.2.0 updates to Font Awsome 5.
-# Note: version specification for shiny-related packages included in relation to the shiny version specification.
-# Note: package V8 in included as per documentation of package shinyjs.
-# It is needed for the use of the the extendShinyjs function;
-# extendShinyjs allows to include JavaScript functions that can be called from R as if they were regular R functions.
+#  (httr has it in the 'Suggests' field only).
+# Note: shiny >= 1.2.0 updates to Font Awesome 5.
+# Note: version specification for shiny-related packages included in relation to
+#  the shiny version specification.
+# Note: package V8 is included as per documentation of the package shinyjs.
+#  It is strongly recommended when using the function extendShinyjs,
+#  which allows to include JavaScript functions that can be called from R
+#  as if they were regular R functions.
 #' @import shiny
 #' @importFrom readr read_csv
 "_PACKAGE"
