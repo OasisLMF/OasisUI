72b6b44ebbea64ae615f4f76f41d96256a9919ba
Sam Gamble <hexadessa@gmail.com>
2019-03-19 16:02:52 +0000

    update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INSTALL_BRANCH='master'
+
+cd $SCRIPT_DIR
+VALID_GIT_REPO="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+
+if [ "$VALID_GIT_REPO" = "true" ]; then
+    INSTALL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+fi 
+
+echo "Building OasisUI from '$INSTALL_BRANCH'"
+
 # build
 docker build -f docker/Dockerfile.oasisui_proxy -t coreoasis/oasisui_proxy .
-docker build -f docker/Dockerfile.oasisui_app --pull -t coreoasis/oasisui_app .
+docker build -f docker/Dockerfile.oasisui_app --pull --build-arg REF_BRANCH=$INSTALL_BRANCH -t coreoasis/oasisui_app .
 
 # run
 docker network create shiny-net
