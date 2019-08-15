7cf07da47733c34f0143b374bb6ccfa2b26d8f6c
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2019-08-15 08:41:38 +0200

    Review createHazardMap. (#162)
    * Technical fixes: Reactive inputs, callModule not inside observer (!).
    * Improve and fix map/markers creation, including better reactive programming.
    * shinycssloaders::withSpinner for the rendering, withModalSpinner for reading the data.
    * Fix mixed module documentation between UI and server functions, the same should be fixed across many other modules, at minimum to not have duplicated `id` param (devtools::check_man()).

--- BFE_RShiny/oasisui/R/createHazardMap_module.R
+++ BFE_RShiny/oasisui/R/createHazardMap_module.R
@@ -1,18 +1,43 @@
+# Mock onclick() with plain observeEvent(). We only use it on buttons and
+# shinyjs::onclick() leads to performance issues for the version we use (fixed
+# in daattali/shinyjs PR #190)
+onclick <- function(id, expr) {
+  parentFrame <- parent.frame(1)
+  session <- shinyjs:::getSession()
+  expr <- substitute(expr)
+  shiny::observeEvent(session$input[[id]], {
+    eval(expr, envir = parentFrame)
+  })
+  invisible(NULL)
+}
+
 # Create Hazard Map module -----------------------------------------------------------
 
+# Shared Module documentation --------------------------------------------------
+#' Hazard Map Module
+#'
+#' Shiny Module for rendering a hazard map using Leaflet.
+#'
+#' @template params-module
+#'
+#' @examples # TODO
+#'
+#' @name createHazardMap
+NULL
+
 # UI ---------------------------------------------------------------------------
-#' createHazardMap
-#'
-#' @rdname createHazardMap
-#'
-#' @description Creates a hazard map using leaflet.
+#' @describeIn createHazardMap Returns the UI elements of the module.
 #'
 #' @export
 createHazardMapUI <- function(id) {
   ns <- NS(id)
 
   tagList(
-    leafletOutput(ns("hazardmap"))
+    shinycssloaders::withSpinner(
+      leafletOutput(ns("hazardmap")),
+      # style and color can be set as options used by all spinners
+      color = "#bb252c"
+    )
     # fluidRow(
     #   sliderInput(ns("contrast_colors"), "Adjust color contrast", min = 0.1, max = 1, value = 0.5)
     # )
@@ -20,17 +45,12 @@ createHazardMapUI <- function(id) {
 }
 
 # Server -----------------------------------------------------------------------
-#' Create Hazard Map
+#' @param map_data Reactive expression yielding Leaflet map data.
+#' @param pins Reactive expression yielding location pins as a \code{data.frame}
+#'   with variables \code{locnumber}, \code{longitude}, \code{latitude}
+#'   (case-insensitive).
 #'
-#' @rdname createHazardMap
-#'
-#' @description Creates a hazard map using leaflet.
-#'
-#' @template params-module
-#' @param file_map File to plot as map.
-#' @param file_pins File to plot pins.
-#'
-#' @return Leaflet map.
+#' @describeIn createHazardMap Defines the server logic of the module.
 #'
 #' @importFrom geojsonio geojson_read
 #' @importFrom leaflet leaflet
@@ -43,18 +63,44 @@ createHazardMapUI <- function(id) {
 #'
 #' @export
 createHazardMap <- function(input, output, session,
-                            file_map,
-                            file_pins) {
+                            map_data,
+                            pins) {
 
-  ns <- session$ns
+  marker_data <- reactive({
+    .build_marker_data(pins())
+  })
 
   # Plot leaflet
   output$hazardmap <- renderLeaflet({
-    .buildHazardMap(file_map, file_pins)
+    # prevent rendering when map_data is unavailable ("falsy"), we would see an
+    # error otherwise (if we would not have the spinner).
+    req(map_data())
+    logMessage("building the map...")
+    map <- .buildHazardMap(map_data(), marker_data())
+    logMessage("map built")
+    map
   })
 
+  # Create marker data
+  .build_marker_data <- function(pins) {
+    data <- pins
+    names(data) <- tolower(names(data))
+    # Popup data, must be a character vector of html code
+    data$popup <- mapply(
+      function(id, lat, lng) {
+        as.character(div(
+          strong("Location ID: "), id,
+          br(), strong("Latitude: "), lat,
+          br(), strong("Longitude: "), lng
+        ))
+      },
+      data$locnumber, data$latitude, data$longitude
+    )
+    data
+  }
+
   # Create map
-  .buildHazardMap <- function(file_map, file_pins) {
+  .buildHazardMap <- function(map_data, marker_data) {
 
     # Create map color palette
     pal <- colorNumeric("Reds", NULL)
@@ -63,35 +109,25 @@ createHazardMap <- function(input, output, session,
     icon_map <- awesomeIcons(
       icon = 'map-marker-alt',
       library = 'fa',
-      iconColor ='green',
-      markerColor =  'blue'
+      iconColor = 'green',
+      markerColor = 'blue'
     )
 
-    names(file_pins) <- tolower(names(file_pins))
-
-    popupData <- tagList(
-      strong("Location ID: "), file_pins$locnumber,
-      br(), strong("Latitude: "), file_pins$latitude,
-      br(), strong("Longitude: "), file_pins$longitude)
-
-    withModalSpinner(
-      hazardmap <- leaflet(file_map) %>%
-        addTiles() %>%
-        addPolygons(color = "transparent",
-                    fillColor = ~pal(file_map$ReturnLevel),
-                    fillOpacity = 1) %>%
-        addLegend(position = "topright",
-                  pal = pal,
-                  values = file_map$ReturnLevel) %>%
-        addAwesomeMarkers(lng = file_pins$longitude,
-                          lat = file_pins$latitude,
-                          icon = icon_map,
-                          clusterOptions = TRUE,
-                          popup = popupData),
-      "Rendering map...",
-      size = "s"
-    )
-
-    return(hazardmap)
+    hazardmap <- leaflet(map_data) %>%
+      addTiles() %>%
+      addPolygons(color = ~pal(ReturnLevel),
+                  fillColor = ~pal(ReturnLevel),
+                  opacity = 0.8,
+                  fillOpacity = 0.5) %>%
+      addLegend(position = "topright",
+                pal = pal,
+                values = ~ReturnLevel) %>%
+      addAwesomeMarkers(lng = marker_data$longitude,
+                        lat = marker_data$latitude,
+                        icon = icon_map,
+                        popup = marker_data$popup,
+                        clusterOptions = TRUE)
+    hazardmap
   }
+
 }
