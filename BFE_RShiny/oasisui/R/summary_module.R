cbd81a2c6f83043bef50d70980f8950087e34294
Nicoletta Farabullini <nicoletta.farabullini@mirai-solutions.com>
2019-11-05 09:20:38 +0100

    bug fix for summary tab plots

--- BFE_RShiny/oasisui/R/summary_module.R
+++ BFE_RShiny/oasisui/R/summary_module.R
@@ -297,9 +297,13 @@ summarytab <- function(input, output, session,
     for (p in 1:length(perspectives)) {
       for (v in 1:length(variables)) {
         variable <- variables[v]
-        fileName <- tbl_filesListDataana %>%
-          filter(summary_level == "All Risks") %>%
-          filter(perspective == perspectives[p]) %>%
+        if (grepl("All Risks", tbl_filesListDataana$summary_level[v])) {
+          fileName <- tbl_filesListDataana %>%
+            filter(summary_level == "All Risks")
+        } else {
+          fileName <- tbl_filesListDataana
+        }
+        fileName <- fileName %>% filter(perspective == perspectives[p]) %>%
           filter(report == variable) %>%
           select(files)
         if (length(fileName$files) > 0) {
