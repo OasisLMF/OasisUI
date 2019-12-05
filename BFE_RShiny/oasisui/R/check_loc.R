9ab9a6b354ae2351ad9c858b268e32e9d89c9c3b
Nicoletta Farabullini <nicoletta.farabullini@mirai-solutions.com>
2019-12-05 11:30:16 +0100

    clean up and updated roxygen

--- BFE_RShiny/oasisui/R/check_loc.R
+++ BFE_RShiny/oasisui/R/check_loc.R
@@ -6,30 +6,24 @@
 #' @param portfolioID selected portfolio ID.
 #' @param data_hub data hub stored in session$userData$data_hub
 #'
-#' @importFrom data.table fread
+#' @return Checked uploaded locations (added peril_id).
+#'
 #' @importFrom dplyr full_join
 #' @importFrom dplyr one_of
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
+#' @importFrom dplyr distinct
 #'
 #' @export
-check_loc <- function(analysisID, portfolioID, data_hub){
-
+check_loc <- function(analysisID, portfolioID, data_hub) {
   logMessage(".check_loc called")
-
-  #initialize df
-  uploaded_locs_check <- NULL
-  uploaded_locs <- NULL
-  modelled_locs <- NULL
 
   uploaded_locs <- data_hub$get_pf_location_content(id = portfolioID) %>%
     mutate(loc_idx = seq(nrow(.)) - 1)
-  modelled_locs <-  data_hub$get_ana_dataset_content(id = analysisID, dataset_identifier = "lookup_success_file")
+  modelled_locs <- data_hub$get_ana_dataset_content(id = analysisID, dataset_identifier = "lookup_success_file")
 
-  #dummy to test validation
-  #modelled_locs <- modelled_locs[1:30,]
   uploaded_locs_check <- full_join(uploaded_locs, modelled_locs, by = "loc_idx") %>%
-    select(-one_of( names(modelled_locs %>% select(-"peril_id")))) %>%
+    select(-one_of(names(modelled_locs %>% select(-"peril_id")))) %>%
     distinct()
 
   return(uploaded_locs_check)
