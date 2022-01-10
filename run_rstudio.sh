a9b4a7567afe782e2c8b26f4d8b1e34fe0a3f4bb
Sam Gamble <hexadessa@gmail.com>
2022-01-10 14:42:06 +0000

    Fix so container can access localhost

--- run_rstudio.sh
+++ run_rstudio.sh
@@ -6,10 +6,7 @@ R_PROJECT_FILE=$SHARED_DIR/'oasisui_dev.Rproj'
 R_CONTAINER_NAME='rstudio_oasisui'
 
 
-# if no project file create a new blank file with default configs 
-
-
-
+# if no project file create a new blank file with default configs
 if [ ! -f "$R_PROJECT_FILE" ]; then
 cat >> $R_PROJECT_FILE <<EOL
 Version: 1.0
@@ -31,22 +28,21 @@ PackageUseDevtools: Yes
 PackagePath: BFE_RShiny/oasisui
 PackageInstallArgs: --no-multiarch --with-keep.source
 EOL
-fi 
-
-
+fi
 
 
 docker stop $R_CONTAINER_NAME && docker rm $R_CONTAINER_NAME
 docker build -f docker/Dockerfile.oasisui_development -t oasisui_dev .
 
 docker run -d \
-  -p 127.0.0.1:$(echo $R_VER | sed 's/[.]//g')0:8787 \
+  #-p 127.0.0.1:$(echo $R_VER | sed 's/[.]//g')0:8787 \
   -e DISABLE_AUTH=true \
   -e USERID=$UID \
   -v $SHARED_DIR:/home/rstudio/oasisui \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  --network="host" \
   --name $R_CONTAINER_NAME \
   oasisui_dev:latest
 
-echo "connect using: http://localhost:4050/"
+echo "connect using: http://localhost:8787/"
