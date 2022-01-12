88cfe0b2f97f5773cb0ba083e520264f63767a66
sambles <sambles@users.noreply.github.com>
2022-01-12 12:40:19 +0000

    Rstudio build image (#275)
    * first pass - rstudio dev setup
    
    * Add Rprofile, and default Rproject files
    
    * Fix so container can access localhost
    
    * Add recommended changes

---
+++ run_rstudio.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+SHARED_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+R_PROJECT_FILE=$SHARED_DIR/'oasisui_dev.Rproj'
+R_CONTAINER_NAME='rstudio_oasisui'
+
+
+# if no project file create a new blank file with default configs
+if [ ! -f "$R_PROJECT_FILE" ]; then
+cat >> $R_PROJECT_FILE <<EOL
+Version: 1.0
+
+RestoreWorkspace: Default
+SaveWorkspace: Default
+AlwaysSaveHistory: Default
+
+EnableCodeIndexing: Yes
+UseSpacesForTab: Yes
+NumSpacesForTab: 2
+Encoding: UTF-8
+
+RnwWeave: Sweave
+LaTeX: pdfLaTeX
+
+BuildType: Package
+PackageUseDevtools: Yes
+PackagePath: BFE_RShiny/oasisui
+PackageInstallArgs: --no-multiarch --with-keep.source
+EOL
+fi
+
+
+docker stop $R_CONTAINER_NAME && docker rm $R_CONTAINER_NAME
+docker build -f docker/Dockerfile.oasisui_development -t oasisui_dev .
+
+docker run -d \
+  -e DISABLE_AUTH=true \
+  -e USERID=$UID \
+  -v $SHARED_DIR:/home/rstudio/oasisui \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  --network="host" \
+  --name $R_CONTAINER_NAME \
+  oasisui_dev:latest
+
+echo "connect using: http://localhost:8787/"
