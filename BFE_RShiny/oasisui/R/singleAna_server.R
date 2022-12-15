5a2542a5f4a0a3e43091b1e9b752566578123c7f
RolandASc <roland.schmid@mirai-solutions.com>
2022-12-15 11:18:08 +0100

    reactivity improvement

--- BFE_RShiny/oasisui/R/singleAna_server.R
+++ BFE_RShiny/oasisui/R/singleAna_server.R
@@ -159,7 +159,7 @@ singleAna <- function(input, output, session,
   # > portfolioID --------------------------------------------------------------
   observeEvent(submodulesList$step1_choosePortfolio$portfolioID(), ignoreInit = TRUE, {
     portfolioID <- submodulesList$step1_choosePortfolio$portfolioID()
-    #Avoid updating input if not necessary
+    # Avoid updating input if not necessary
     if (!is.na(portfolioID) && result$portfolioID != portfolioID) {
       logMessage(paste0("updating result$portfolioID because submodulesList$step1_choosePortfolio$portfolioID() changed to: ", portfolioID ))
       result$portfolioID <- portfolioID
@@ -168,23 +168,23 @@ singleAna <- function(input, output, session,
 
   observeEvent(selectPortfolioID(),  ignoreInit = TRUE, {
     portfolioID <- selectPortfolioID()
-    #Avoid updating input if not necessary
+    # Avoid updating input if not necessary
     if (!is.na(portfolioID) && portfolioID != "" && result$portfolioID != portfolioID) {
       logMessage(paste0("updating result$portfolioID becauseselectPortfolioID() changed to: ", portfolioID ))
       result$portfolioID <- portfolioID
     }
   })
 
-  observeEvent(input$portfolioID, ignoreInit = TRUE,{
-    #Avoid updating input if not necessary
+  observeEvent(input$portfolioID, ignoreInit = TRUE, {
+    # Avoid updating input if not necessary
     if (input$portfolioID != result$portfolioID) {
       logMessage(paste0("updating result$portfolioID because input$portfolioID changed to: ", input$portfolioID ))
       result$portfolioID <- input$portfolioID
     }
   })
 
-  observeEvent(result$portfolioID, ignoreInit = TRUE,{
-    #Avoid updating input if not necessary
+  observeEvent(result$portfolioID, ignoreInit = TRUE, {
+    # Avoid updating input if not necessary
     if (input$portfolioID != result$portfolioID) {
       logMessage(paste0("updating input$portfolioID because result$portfolioID changed to: ", result$portfolioID ))
       updateSelectizeInput(session, inputId = "portfolioID", selected = result$portfolioID, choices = result$pfChoices)
