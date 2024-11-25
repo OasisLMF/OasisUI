f0a2e8e559bbd7954b1eaed9ab310e69482b55a7
Davide Vitiello <davide.vitiello@mirai-solutions.com>
2024-11-25 15:29:56 +0100

    Add "appli" UI runner using `mode` parameter

--- BFE_RShiny/oasisui/R/runOasiui.R
+++ BFE_RShiny/oasisui/R/runOasiui.R
@@ -7,13 +7,15 @@
 #' directory and is available under the specified port.
 #'
 #' @param port port to pass to \code{\link[shiny]{runApp}}.
+#' @param mode directory of BFE_RShiny/oasisui to run the app from. Currently,
+#' "app" (full application) and "appli" (mini application)
 #' @param ... Additional arguments to \code{\link[shiny]{runApp}}.
 #'
 #' @return No return value.
 #'
 #' @export
-runOasisui <- function(port = 3838, ...) {
+runOasisui <- function(port = 3838, mode = "app", ...) {
 
-  runApp(appDir = system.file("app", package = "oasisui"), port = port, ...)
+  runApp(appDir = system.file(mode, package = "oasisui"), port = port, ...)
 
 }
