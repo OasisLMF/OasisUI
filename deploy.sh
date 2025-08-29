8f654a39f4def9ee5fdf49c4695b250c9bb10158
sambles <sambles@users.noreply.github.com>
2025-08-29 10:10:52 +0100

    Update OasisUI rocker image for CVE (#328)
    * UI 1.12.0 - Upgrade all os packages and use base img rocker 4.5
    
    try changes from Darren
    
    fix CI
    
    set version 1.12.0
    
    fix missing dep
    
    Seems to be working?
    
    base image not enough to cover CVE
    
    pip3 install python deps
    
    update trivy CVE scanner
    
    fix copy pasta
    
    more pasta
    
    back up a ver
    
    fix
    
    Upgrade all os packages and test rocker 4.5
    
    Update CVE job
    
    * rocker 4.4
    
    * install revgeo from github
    
    * try build in CI
    
    * replace rgdal
    
    * test fix
    
    * syntax
    
    * revert and build with shinyjs 1.1
    
    * ffs
    
    * downgrade tidyr
    
    * tested and working on 1.28.14

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+set -e 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-INSTALL_BRANCH='master'
+INSTALL_BRANCH='main'
 
 cd $SCRIPT_DIR
 VALID_GIT_REPO="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
@@ -13,8 +14,8 @@ fi
 echo "Building OasisUI from '$INSTALL_BRANCH'"
 
 # build
-docker build -f docker/Dockerfile.oasisui_proxy -t coreoasis/oasisui_proxy .
-docker build -f docker/Dockerfile.oasisui_app --pull --build-arg REF_BRANCH=$INSTALL_BRANCH -t coreoasis/oasisui_app .
+#docker build -f docker/Dockerfile.oasisui_proxy -t coreoasis/oasisui_proxy .
+docker build --no-cache --progress=plain -f docker/Dockerfile.oasisui_app --pull --build-arg REF_BRANCH=$INSTALL_BRANCH -t coreoasis/oasisui_app .
 
 # run
 docker network create shiny-net
